2a7dfe1a32e3ba2a1b09948d2cdf257c429dbd09
rtadarsh <adarshramtripathi123@gmail.com>
2021-12-21 16:29:32 +0530

    Day 2 Completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,11 +5,22 @@ class HomePage extends StatelessWidget {
   int days = 30;
   String name = "Adarsh";
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Container(
-          child: Text('Welcome to $days days of flutter by $name'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF8EFFEE),
+        title: Text(
+          "First Flutter App",
+          style: TextStyle(fontSize: 20, color: Color(0xFF000000)),
+        ),
+      ),
+      body: Center(
+          child: Container(
+        child: Text('Welcome to $days days of flutter by $name'),
       )),
+      drawer: Drawer(
+        backgroundColor: Color(0xFF11111),
+        child: ListView(),
+      ),
     );
   }
 }
